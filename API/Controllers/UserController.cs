4c25f4bb59747d2b695966fe70830a5ee7d63096
thiagobcf <thiagobcfonseca@gmail.com>
2023-12-26 13:26:15 -0300

    add new API Controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")] //  /api/users
+public class UsersController: ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        
+        return users;
+    }
+
+    [HttpGet("{id}")]   //  /api/users/id
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+    }
+}
