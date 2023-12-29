54d339ad4142e6f3be06bc943bda82eb187434bb
thiagobcf <thiagobcfonseca@gmail.com>
2023-12-29 10:50:51 -0300

    update user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,10 @@
-namespace API.Entities;
-public class AppUser
+namespace API.Entities
 {
-    public int Id { get; set; }
-    public string UserName { get; set;}
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
 }
