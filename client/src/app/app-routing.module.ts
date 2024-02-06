c6be07ebcfffd7fb375567b5adb7d7b9429ca77e
thiagobcf <thiagobcfonseca@gmail.com>
2024-02-06 12:57:24 -0300

    add dummy route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,14 +4,20 @@ import { HomeComponent } from './home/home.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { authGuard } from './_guards/auth.guard';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberDetailComponent, canActivate:[authGuard]},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberDetailComponent},
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+    ]
+  },  
   {path: '**', component: HomeComponent, pathMatch: 'full'},   // qq coisa que nao der match com as rotas acima vai parar aqui
 ];
 
