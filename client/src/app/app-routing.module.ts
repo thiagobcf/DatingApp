70cbe30d70aca111b4328c2b415303214cfe2c9f
thiagobcf <thiagobcfonseca@gmail.com>
2024-02-06 10:33:32 -0300

    Create components: members, list,messages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'members', component: MemberDetailComponent},
+  {path: 'members/:id', component: MemberDetailComponent},
+  {path: 'lists', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'},   // qq coisa que nao der match com as rotas acima vai parar aqui
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
