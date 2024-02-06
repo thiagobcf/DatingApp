3a8141084d6de0cebb960561196a18f0e4d8c238
thiagobcf <thiagobcfonseca@gmail.com>
2024-02-06 12:18:10 -0300

    add toast service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -31,8 +32,12 @@ import { MessagesComponent } from './messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
-    FormsModule
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    
   ],
   providers: [
     provideClientHydration()
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -3,6 +3,7 @@ import { EventEmitter } from '@angular/core'
 import { AccountService } from '../_services/account.service';
 import { response } from 'express';
 import { error } from 'console';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
   
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
       
@@ -24,7 +25,10 @@ export class RegisterComponent implements OnInit {
       next: () => {        
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error),
+        console.log(error)
+      }
     })
   }
 
