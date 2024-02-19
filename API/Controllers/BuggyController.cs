235663211a9e87d2554920d4a6461331cf0f5d1b
thiagobcf <thiagobcfonseca@gmail.com>
2024-02-19 12:46:01 -0300

    add Exception handling middleware

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -33,11 +33,15 @@ namespace API.Controllers
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
+            
             var thing = _context.Users.Find(-1);
 
             var thingToReturn = thing.ToString();
 
             return thingToReturn;
+            
+            
+            
         }
 
         [HttpGet("bad-request")]
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,5 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,10 +12,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("https://localhost:4200"));
