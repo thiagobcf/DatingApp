61689819a16a1edd02b53ac02554c1712dca8e88
thiagobcf <thiagobcfonseca@gmail.com>
2024-02-29 13:14:37 -0300

    add: Creating a repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.interfaces;
 using API.Services;
@@ -20,6 +16,7 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
---
+++ API/interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+
+namespace API.interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>>GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+    }
+}
