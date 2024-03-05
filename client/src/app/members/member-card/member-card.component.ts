2c281b72b0c036a45320746c597407ccbe2c174f
thiagobcf <thiagobcfonseca@gmail.com>
2024-03-05 19:01:07 -0300

    add: animated buttons

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrl: './member-card.component.css'
+  styleUrl: './member-card.component.css',  
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
