be7f47e08797b14ff76704580b292fd460d3b839
thiagobcf <thiagobcfonseca@gmail.com>
2024-03-06 10:23:16 -0300

    add: interceptor to send the token

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, take } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private accountService: AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>>{
+    // qdo tiver obtido o que queremos essa assinatura sera concluida e não mais consume recursos do aplicativo
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => {
+        if (user) {
+          request = request.clone({
+            setHeaders: {
+              Authorization: `Bearer ${user.token}`
+            }
+          })
+        }
+      }
+    })          
+    return next.handle(request);
+  }
+}
