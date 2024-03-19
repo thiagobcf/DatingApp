91cfdc0bf61f3ba92f1f8e40035e709032975d67
thiagobcf <thiagobcfonseca@gmail.com>
2024-03-19 11:16:54 -0300

    add: Reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
@@ -44,6 +44,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
     
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -4,32 +4,44 @@ import { AccountService } from '../_services/account.service';
 import { response } from 'express';
 import { error } from 'console';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {  
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
   
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
-      
+      this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username : new FormControl(),
+      password : new FormControl(),
+      confirmPassword : new FormControl(),
+    })
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {        
-        this.cancel();
-      },
-      error: error => {
-        this.toastr.error(error.error),
-        console.log(error)
-      }
-    })
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {        
+    //     this.cancel();
+    //   },
+    //   error: error => {
+    //     this.toastr.error(error.error),
+    //     console.log(error)
+    //   }
+    // })
   }
 
   cancel() {
