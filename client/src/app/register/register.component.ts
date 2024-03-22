797447875936a6f5abcfcb37356644f52f60c911
thiagobcf <thiagobcfonseca@gmail.com>
2024-03-22 12:49:43 -0300

    add: Client side registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -6,6 +6,7 @@ import { error } from 'console';
 import { ToastrService } from 'ngx-toastr';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ThisReceiver } from '@angular/compiler';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,12 +15,12 @@ import { ThisReceiver } from '@angular/compiler';
 })
 export class RegisterComponent implements OnInit {  
   @Output() cancelRegister = new EventEmitter();
-  model: any = {}
   registerForm: FormGroup = new FormGroup({});
   maxDate: Date = new Date();
+  validationErrors: string[] | undefined;
   
   constructor(private accountService: AccountService, private toastr: ToastrService, 
-    private fb: FormBuilder) {}
+    private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
       this.initializeForm();
@@ -49,20 +50,29 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  register() {
-    console.log(this.registerForm?.value);
-    // this.accountService.register(this.model).subscribe({
-    //   next: () => {        
-    //     this.cancel();
-    //   },
-    //   error: error => {
-    //     this.toastr.error(error.error),
-    //     console.log(error)
-    //   }
-    // })
+  register() {   
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const values = {...this.registerForm.value, dateOfBirth: dob};
+    // console.log(values);
+    this.accountService.register(values).subscribe({
+      next: () => {        
+        this.router.navigateByUrl('/members')
+      },
+      error: error => {        
+        this.validationErrors = error
+      }
+    })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  // dob: dateOfBirth
+  private getDateOnly(dob: string | undefined){
+    if (!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+      .toISOString().slice(0,10);                 // setando para hora local
   }
 }
