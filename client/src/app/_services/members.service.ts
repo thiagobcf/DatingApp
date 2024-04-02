bd28328fff8abba75a5fe6f8a5786e8c12b5d8a2
thiagobcf <thiagobcfonseca@gmail.com>
2024-04-02 10:57:18 -0300

    add: Restoring caching for member detail

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,8 +13,7 @@ import { UserParams } from '../_models/userParams';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-  memberCache = new Map();
-  
+  memberCache = new Map();  
 
   constructor(private http: HttpClient) { }
 
@@ -38,6 +37,35 @@ export class MembersService {
         return response;
       })
     )
+  }
+
+  getMember(username: string) {
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+
+    if (member) {
+      return of(member);
+    }
+    
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = { ...this.members[index], ...member }
+      })
+    )
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 
   private getPaginatedResult<T>(url: string, params: HttpParams) {
@@ -65,28 +93,4 @@ export class MembersService {
 
     return params;
   }
-
-  getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
-    if (member) return of(member);
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-
-  updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        this.members[index] = { ...this.members[index], ...member }
-      })
-    )
-  }
-
-  setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
-  }
-
-  deletePhoto(photoId: number) {
-    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
-  }
-
 }
