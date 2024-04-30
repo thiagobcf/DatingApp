b524b6e0e620e6e8fc6c400720cf671a84c79c0d
thiagobcf <thiagobcfonseca@gmail.com>
2024-04-30 12:37:15 -0300

    add: Dealing with UTC date formats

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -20,7 +20,13 @@ namespace API.Helpers
                 .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos
                     .FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(d => d.RecipentPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos
-                    .FirstOrDefault(x => x.IsMain).Url));                
+                    .FirstOrDefault(x => x.IsMain).Url)); 
+
+            CreateMap<DateTime, DateTime>()
+                .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc)); 
+            CreateMap<DateTime?, DateTime?>()
+                .ConvertUsing(d => d.HasValue ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);             
+
         }
     }
 }
